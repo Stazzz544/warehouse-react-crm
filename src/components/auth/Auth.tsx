dabc17b52e4e40812531fbcb0afeb6a488390839
Basilevs <stazzzdzr@gmail.com>
2022-05-23 22:12:41 +0300

    img-added

--- src/components/auth/Auth.tsx
+++ src/components/auth/Auth.tsx
@@ -4,22 +4,34 @@ import './styles/Auth.scss'
 
 const Auth = () => {
 	const [isNewUser, setIsNewUser] = useState(false)
+
 	return (
 		<div className="auth">
 			<div className="auth__wrapper">
-				<button 
-				onClick={() => setIsNewUser(false)}
-				className="auth__signIn">Войти</button>
-				<button 
-				onClick={() => setIsNewUser(true)}
-				className="auth__signUp">Зарегестрироваться</button>
-				<div className="auth__form">
-					{isNewUser ?
-					<>yes</>
-					:
-						<SingIn/>
-					}
+				<div className="auth__left-side">
+
 				</div>
+
+				<div className="auth__right-side">
+					<div className="auth__form">
+						{isNewUser ?
+							<>yes</>gi
+							:
+							<SingIn />
+						}
+						<button
+							onClick={() => setIsNewUser(false)}
+							className="auth__signIn">Войти</button>
+
+						<button
+							onClick={() => setIsNewUser(true)}
+							className="auth__signUp">Регистрация</button>
+
+					</div>
+				</div>
+
+
+
 			</div>
 		</div>
 
